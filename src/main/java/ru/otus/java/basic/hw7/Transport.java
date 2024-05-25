b85638e7c9cea9f8c71dd6491c3be4c373df61e7
DARK-WAY <o_kv@mail.ru>
2024-05-25 22:56:39 +0300

    Домашнее задание к Уроку 13. Переработанная версия.

--- src/main/java/ru/otus/java/basic/hw7/Transport.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.java.basic.hw7;
-
-public interface Transport { // маршрут
-    String getFullName(); // наименование транспорта
-
-    boolean move(Obstacle obstacle, Human human); // передвижение
-}
