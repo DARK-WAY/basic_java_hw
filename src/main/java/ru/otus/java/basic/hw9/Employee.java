b007c413111f4aa883317fd28b14feeaeebc65a9
DARK-WAY <o_kv@mail.ru>
2024-05-27 01:43:20 +0300

    Домашнее задание к Уроку 15

---
+++ src/main/java/ru/otus/java/basic/hw9/Employee.java
@@ -0,0 +1,20 @@
+package ru.otus.java.basic.hw9;
+
+public class Employee {
+    private final String name;
+    private final int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+ }
