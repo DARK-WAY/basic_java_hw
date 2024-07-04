78212061b0c51ad3bbb238e0c05559c66e26f2e1
DARK-WAY <o_kv@mail.ru>
2024-07-04 15:29:29 +0300

    Домашнее задание  к Уроку 20

---
+++ src/main/java/ru/otus/java/basic/hw13/Server.java
@@ -0,0 +1,54 @@
+package ru.otus.java.basic.hw13;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Сервер стартанул.");
+        ServerSocket serverSocket = new ServerSocket(8555);
+        Socket socket = serverSocket.accept();
+        System.out.println("Клиент подключился.");
+
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        out.writeUTF("Доступны следующие математические операции: +,-,*,/ .");
+
+        // Ждем задание + его выполняем
+        String result;
+        Float parametr1 = Float.parseFloat(in.readUTF());
+        System.out.println(parametr1);
+
+        String operation = in.readUTF();
+        System.out.println(operation);
+
+        Float parametr2 = Float.parseFloat(in.readUTF());
+        System.out.println(parametr2);
+
+        System.out.println("Сервер получил задание.");
+
+        if (operation.equals("+")) {
+            result = String.valueOf(parametr1 + parametr2);
+        } else if (operation.equals("-")) {
+            result = String.valueOf(parametr1 - parametr2);
+        } else if (operation.equals("/")) {
+            if (parametr2 != 0f) {
+                result = String.valueOf(parametr1 / parametr2);
+            } else {
+                result = "Деление  на 0 -  запрещено";
+            }
+        } else if (operation.equals("*")) {
+            result = String.valueOf(parametr1 * parametr2);
+        } else result = "Не кеорректный математический знак: " + operation;
+
+        out.writeUTF(result);
+        System.out.println("Сервер отправилд результат.");
+
+        System.out.println("Сервер отключается.");
+        socket.close();
+        serverSocket.close();
+    }
+}
